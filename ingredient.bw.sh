d66a7fd76210be61272d1c236f246b6df3076c44
Ross Payne <rosshpayne@gmail.com>
2018-12-28 05:38:37 +0000

    data for previous commit

---
+++ ingredient.bw.sh
@@ -0,0 +1,5 @@
+aws dynamodb batch-write-item \
+    --request-items file://data/ingredient.data.1.json \
+    --return-consumed-capacity TOTAL
+    
+
---
+++ ingredient.sh
@@ -0,0 +1,2 @@
+aws dynamodb create-table  --cli-input-json file://ingredient.create.json
+        
