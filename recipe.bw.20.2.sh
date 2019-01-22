ea34c33a2b7b338200ef85c0ea384356ac080613
Ross Payne <rosshpayne@gmail.com>
2019-01-22 03:35:57 +0000

    dynoamodb load script

---
+++ recipe.bw.20.2.sh
@@ -0,0 +1,5 @@
+aws dynamodb batch-write-item \
+    --request-items file://data/activity.data.20.2.json \
+    --return-consumed-capacity TOTAL
+    
+
---
+++ recipe.bw.21.9.sh
@@ -0,0 +1,5 @@
+aws dynamodb batch-write-item \
+    --request-items file://data/activity.data.21.9.json \
+    --return-consumed-capacity TOTAL
+    
+
