2cd080f248ea987595aff2c6e25de5f89141e201
Ross Payne <rosshpayne@gmail.com>
2019-04-30 05:03:39 +0000

    data files

--- activity.bw.3.sh
+++
@@ -1,4 +0,0 @@
-
-aws dynamodb batch-write-item \
-    --request-items file://data/activity.data.21.3.json \
-    --return-consumed-capacity TOTAL
--- recipe.bw.20.7.sh
+++ recipe.bw.20.7.sh
@@ -1,8 +1,8 @@
 aws dynamodb batch-write-item \
     --request-items file://data/activity.data.20.7.json \
     --return-consumed-capacity TOTAL
-    
+  
 aws dynamodb batch-write-item \
     --request-items file://data/activity.data.20.7a.json \
-    --return-consumed-capacity TOTAL
+    --return-consumed-capacity TOTAL  
 
