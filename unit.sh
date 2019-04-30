98dfb32bbe0adef63d4150f6d0c9f5f86e095f9a
Ross Payne <rosshpayne@gmail.com>
2019-04-30 05:16:12 +0000

    tidy up

--- unit.sh
+++
@@ -1,8 +0,0 @@
-aws dynamodb create-table \
-    --table-name Unit \
-    --attribute-definitions \
-        AttributeName=slabel,AttributeType=S \
-    --key-schema \
-        AttributeName=slabel,KeyType=HASH \
-    --provisioned-throughput \
-        ReadCapacityUnits=1,WriteCapacityUnits=1
